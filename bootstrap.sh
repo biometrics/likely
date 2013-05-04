71c46c048b9e8dd3050f157de735e01b9e6e240a
Josh Klontz <josh.klontz@gmail.com>
2013-05-04 17:54:09 -0400

    boostrap.sh tweaks

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,16 +4,19 @@ if NOT hash cmake 2>/dev/null; then
   exit
 fi
 
+THREADS=9
+
 rm -rf build
 mkdir build
 cd build
 cmake ..
-make -j
+make -j${THREADS}
 cmake ..
-make -j
+make -j${THREADS}
 cmake -DBUILD_TESTING=ON ..
-make -j
+make -j${THREADS}
 cmake ..
-make -j
-make install
+make -j${THREADS}
+make test
 cpack -G TGZ
+
