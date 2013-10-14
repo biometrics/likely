e7ba5693e4a48c4bdfe88e995f0fe92eb8211345
Josh Klontz <Joshua.Klontz@noblis.org>
2013-10-14 10:38:19 -0400

    removed deprecated widget components

--- dream.cpp
+++ dream.cpp
@@ -17,104 +17,8 @@
 #include <QtCore>
 #include <QtWidgets>
 #include <lua.hpp>
+
 #include "likely.h"
-
-class MatrixViewer : public QLabel
-{
-    Q_OBJECT
-    QImage src;
-    int zoomLevel = 0;
-
-public:
-    explicit MatrixViewer(QWidget *p = 0) : QLabel(p)
-    {
-        setAcceptDrops(true);
-        setAlignment(Qt::AlignCenter);
-        setMouseTracking(true);
-        updatePixmap();
-    }
-
-public slots:
-    void setImage(const QImage &i)             { src =  i;      updatePixmap(); }
-    void zoomIn()  { if (++zoomLevel >  4) zoomLevel =  4; else updatePixmap(); }
-    void zoomOut() { if (--zoomLevel < -4) zoomLevel = -4; else updatePixmap(); }
-
-private:
-    void dragEnterEvent(QDragEnterEvent *e)
-    {
-        e->accept();
-        if (e->mimeData()->hasUrls())
-            e->acceptProposedAction();
-    }
-
-    void dropEvent(QDropEvent *e)
-    {
-        e->accept();
-        e->acceptProposedAction();
-        const QMimeData *md = e->mimeData();
-        if (md->hasUrls()) {
-            foreach (const QUrl &url, md->urls()) {
-                if (!url.isValid()) continue;
-                const QString file = url.toLocalFile();
-                if (file.isNull()) continue;
-                QAction *action = new QAction(this);
-                action->setData(file);
-                emit newMatrix(action);
-                break;
-            }
-        }
-    }
-
-    void leaveEvent(QEvent *e)
-    {
-        e->accept();
-        queryPoint(QPoint(-1, -1));
-    }
-
-    void mouseMoveEvent(QMouseEvent *e)
-    {
-        e->accept();
-        queryPoint(e->pos() / pow(2, zoomLevel));
-    }
-
-    void updatePixmap()
-    {
-        if (src.isNull()) {
-            clear();
-            setText("<b>Drag and Drop Image Here</b>");
-            resize(512, 512);
-            queryPoint(QPoint(-1, -1));
-            setFrameShape(QFrame::StyledPanel);
-        } else { const QSize newSize = src.size() * pow(2, zoomLevel);
-            setPixmap(QPixmap::fromImage(src.scaled(newSize, Qt::KeepAspectRatio)));
-            resize(newSize);
-            queryPoint(mapFromGlobal(QCursor::pos()) / pow(2, zoomLevel));
-            setFrameShape(QFrame::NoFrame);
-        }
-    }
-
-    void queryPoint(const QPoint &p)
-    {
-        if (src.rect().contains(p)) {
-            const QRgb pixel = src.pixel(p);
-            emit newPosition(QString("%1,%2")
-                             .arg(QString::number(p.x()),
-                                  QString::number(p.y())));
-            emit newColor(QString("<font color=\"red\">%1</font>,<font color=\"green\">%2</font>,<font color=\"blue\">%3</font>")
-                          .arg(QString::number(qRed(pixel)),
-                               QString::number(qGreen(pixel)),
-                               QString::number(qBlue(pixel))));
-        } else {
-            emit newPosition("");
-            emit newColor("");
-        }
-    }
-
-signals:
-    void newMatrix(QAction*);
-    void newPosition(QString);
-    void newColor(QString);
-};
 
 class ErrorHandler : public QObject
 {
@@ -311,43 +215,6 @@ signals:
     void newInfo(QString);
 };
 
-class StatusLabel : public QLabel
-{
-    Q_OBJECT
-    QString description;
-
-public:
-    StatusLabel(const QString &description_) : description(description_)
-    {
-        setToolTip(description);
-        setDescription();
-    }
-
-public slots:
-    void setText(const QString &text)
-    {
-        if (text.isEmpty()) {
-            setDescription();
-        } else {
-            setEnabled(true);
-            QLabel::setText(text);
-        }
-    }
-
-private:
-    void setDescription()
-    {
-        setEnabled(false);
-        QLabel::setText(description);
-    }
-
-    void resizeEvent(QResizeEvent *e)
-    {
-        e->accept();
-        setMinimumWidth(width());
-    }
-};
-
 int main(int argc, char *argv[])
 {
     for (int i=1; i<argc; i++) {
@@ -375,66 +242,31 @@ int main(int argc, char *argv[])
     QApplication::setOrganizationDomain("liblikely.org");
     QApplication application(argc, argv);
 
-    QMenu *sourceMenu = new QMenu("Source");
-    QAction *openSource = new QAction("Open...", sourceMenu);
-    QAction *saveSource = new QAction("Save", sourceMenu);
-    QAction *saveSourceAs = new QAction("Save As...", sourceMenu);
+    QMenu *fileMenu = new QMenu("File");
+    QAction *openSource = new QAction("Open...", fileMenu);
+    QAction *saveSource = new QAction("Save", fileMenu);
+    QAction *saveSourceAs = new QAction("Save As...", fileMenu);
     openSource->setShortcut(QKeySequence("Ctrl+O"));
     saveSource->setShortcut(QKeySequence("Ctrl+S"));
     saveSourceAs->setShortcut(QKeySequence("Ctrl+Shift+S"));
-    sourceMenu->addAction(openSource);
-    sourceMenu->addAction(saveSource);
-    sourceMenu->addAction(saveSourceAs);
+    fileMenu->addAction(openSource);
+    fileMenu->addAction(saveSource);
+    fileMenu->addAction(saveSourceAs);
 
     Editor *editor = new Editor();
-    QObject::connect(sourceMenu, SIGNAL(triggered(QAction*)), editor, SLOT(setSource(QAction*)));
-
-    MatrixViewer *matrixViewer = new MatrixViewer();
-
-    QMenu *viewMenu = new QMenu("View");
-    QAction *zoomIn = new QAction("Zoom In", viewMenu);
-    QAction *zoomOut = new QAction("Zoom Out", viewMenu);
-    zoomIn->setShortcut(QKeySequence("Ctrl++"));
-    zoomOut->setShortcut(QKeySequence("Ctrl+-"));
-    viewMenu->addAction(zoomIn);
-    viewMenu->addAction(zoomOut);
-    QObject::connect(zoomIn, SIGNAL(triggered()), matrixViewer, SLOT(zoomIn()));
-    QObject::connect(zoomOut, SIGNAL(triggered()), matrixViewer, SLOT(zoomOut()));
+    QObject::connect(fileMenu, SIGNAL(triggered(QAction*)), editor, SLOT(setSource(QAction*)));
 
     QMenuBar *menuBar = new QMenuBar();
-    menuBar->addMenu(sourceMenu);
-    menuBar->addMenu(viewMenu);
+    menuBar->addMenu(fileMenu);
 
     QGridLayout *centralWidgetLayout = new QGridLayout();
-    QScrollArea *matrixViewerScrollArea = new QScrollArea();
-    matrixViewerScrollArea->setAlignment(Qt::AlignCenter);
-    matrixViewerScrollArea->setFrameShape(QFrame::NoFrame);
-    matrixViewerScrollArea->setWidget(matrixViewer);
     centralWidgetLayout->addWidget(editor, 0, 0);
-    centralWidgetLayout->addWidget(matrixViewerScrollArea, 0, 1, 2, 1);
     QWidget *centralWidget = new QWidget();
     centralWidget->setLayout(centralWidgetLayout);
-
-    StatusLabel *info = new StatusLabel("info");
-    StatusLabel *hash = new StatusLabel("hash");
-    StatusLabel *dimensions = new StatusLabel("dimensions");
-    StatusLabel *position = new StatusLabel("position");
-    StatusLabel *color = new StatusLabel("color");
-    QObject::connect(editor, SIGNAL(newInfo(QString)), info, SLOT(setText(QString)));
-    QObject::connect(matrixViewer, SIGNAL(newPosition(QString)), position, SLOT(setText(QString)));
-    QObject::connect(matrixViewer, SIGNAL(newColor(QString)), color, SLOT(setText(QString)));
-    QStatusBar *statusBar = new QStatusBar();
-    statusBar->addPermanentWidget(info);
-    statusBar->addPermanentWidget(new QWidget(), 1);
-    statusBar->addPermanentWidget(hash);
-    statusBar->addPermanentWidget(dimensions);
-    statusBar->addPermanentWidget(position);
-    statusBar->addPermanentWidget(color);
 
     QMainWindow mainWindow;
     mainWindow.setCentralWidget(centralWidget);
     mainWindow.setMenuBar(menuBar);
-    mainWindow.setStatusBar(statusBar);
     mainWindow.setWindowTitle("Likely Dream");
     mainWindow.resize(800,600);
     mainWindow.show();
