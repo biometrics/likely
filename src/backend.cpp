90f1888e16f628730ed4b6ed8ad41990a4f0bde9
Josh Klontz <Joshua.Klontz@noblis.org>
2014-03-24 10:34:39 -0400

    set expression now returns RHS instead of LHS

--- src/backend.cpp
+++ src/backend.cpp
@@ -1029,13 +1029,14 @@ class setExpression : public Operator
         if (ast->atoms[1]->is_list)
             return error(ast->atoms[1], "expected an atom");
         const string name = ast->atoms[1]->atom;
-        TRY_EXPR(builder, ast->atoms[2], expr);
-
-        Immediate &variable = builder.locals[name];
-        if (variable.isNull())
-            variable = Immediate(builder.CreateAlloca(expr.value()->getType(), 0, name), expr);
-        builder.CreateStore(expr, variable);
-        return new Immediate(variable);
+        Expression *expr = builder.expression(ast->atoms[2]);
+        if (expr) {
+            Immediate &variable = builder.locals[name];
+            if (variable.isNull())
+                variable = Immediate(builder.CreateAlloca(expr->value()->getType(), 0, name), *expr);
+            builder.CreateStore(*expr, variable);
+        }
+        return expr;
     }
 };
 LIKELY_REGISTER(set)
