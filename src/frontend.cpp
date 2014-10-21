d7c11caf32439f9697ced0bc914a6886b60d921f
Josh Klontz <josh.klontz@gmail.com>
2014-10-21 11:54:37 -0400

    fixed one more msvc warning

--- src/frontend.cpp
+++ src/frontend.cpp
@@ -14,6 +14,10 @@
  * limitations under the License.                                            *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+#ifdef _MSC_VER
+#  define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #include <algorithm>
 #include <cassert>
 #include <cstdarg>
