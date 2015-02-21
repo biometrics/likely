4c9f1fe160da3ba5bdd23d509e8b195d9653c81a
Josh Klontz <josh.klontz@gmail.com>
2015-02-21 16:25:51 -0500

    added missing file

---
+++ src/cuda.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cuda/cuda.h>
+
+#include "likely/runtime.h"
+
+static CUdevice Device;
+
+bool likely_initialize_coprocessor()
+{
+    if (cuInit(0))
+        return false;
+
+    int deviceCount;
+    if(cuDeviceGetCount(&deviceCount))
+        return false;
+
+    if (deviceCount == 0)
+        return false;
+
+    if (cuDeviceGet(&Device, 0))
+        return false;
+
+    return true;
+}
