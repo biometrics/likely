8a5fa63d2d81200e34c65513a5c72171bd3793c2
Josh Klontz <josh.klontz@gmail.com>
2016-04-26 16:48:11 -0400

    removed PCA-related library files

--- library/PCA-test.lisp
+++
@@ -1,9 +0,0 @@
-"library/PCA.md".import
-
-mnist := "data/mnist/train-images-idx3-ubyte.lm".read-matrix
-original-rows    := mnist.rows
-original-columns := mnist.columns
-
-mnist := (u8XY 1 (* mnist.columns mnist.rows) mnist.frames 1 mnist.data)
-
-(PCA mnist)
